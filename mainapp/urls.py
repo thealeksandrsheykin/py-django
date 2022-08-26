0feb57f8740efe49b37afe1ae804f0b10d54625c
Aleksandr <alexander_sheykin@mail.ru>
2022-08-26 15:50:56 +0300

    Lesson 2 Static site with Django

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -4,8 +4,19 @@ from mainapp.apps import MainappConfig
 
 app_name = MainappConfig.name
 
+# urlpatterns = [
+#     path('',views.HelloWorldView.as_view()),
+#     path('<str:word>/',views.check_kwargs),
+
+# ]
+
 urlpatterns = [
-    path('',views.HelloWorldView.as_view()),
-    path('<str:word>/',views.check_kwargs),
+    path('',views.MainPageView.as_view()),
+    path('news/', views.NewsPageView.as_view()),
+    path('coursers/', views.CoursesPageView.as_view()),
+    path('contacts/', views.ContactsPageView.as_view()),
+    path('doc_site/', views.DocSitePageView.as_view()),
+    path('login/', views.LoginPageView.as_view()),
+
 
 ]
--- mainapp/views.py
+++ mainapp/views.py
@@ -1,10 +1,9 @@
 from django.http import HttpResponse
 from django.views.generic import View
+from django.views.generic import TemplateView
 
 #  FBV
-
 # from django.shortcuts import render
-# from django.http import HttpResponse
 
 # # Create your views here.
 
@@ -16,12 +15,28 @@ def check_kwargs(request,**kwargs):
 
 
 #CBV
+class MainPageView(TemplateView):
+    template_name = "mainapp/index.html"
 
 
+class NewsPageView(TemplateView):
+    template_name = "mainapp/news.html"
 
-class HelloWorldView(View):
-    def get(self, *args, **kwargs):
-        return HttpResponse('Hello World')
+
+class CoursesPageView(TemplateView):
+    template_name = "mainapp/courses_list.html"
+
+
+class ContactsPageView(TemplateView):
+    template_name = "mainapp/contacts.html"
+
+
+class DocSitePageView(TemplateView):
+    template_name = "mainapp/doc_site.html"
+
+
+class LoginPageView(TemplateView):
+    template_name = "mainapp/login.html"
 
 
 
