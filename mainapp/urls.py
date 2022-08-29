afac8258d53f7e8e9a627e93a354d041ef8cc494
Aleksandr <alexander_sheykin@mail.ru>
2022-08-29 14:47:16 +0300

    Fixed remarks lesson-2 (add namespace for path and when calling the link add {% url namespace:url-path %})

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -12,10 +12,10 @@ app_name = MainappConfig.name
 # ]
 
 urlpatterns = [
-    path("", views.MainPageView.as_view()),
-    path("news/", views.NewsPageView.as_view()),
-    path("coursers/", views.CoursesPageView.as_view()),
-    path("contacts/", views.ContactsPageView.as_view()),
-    path("doc_site/", views.DocSitePageView.as_view()),
-    path("login/", views.LoginPageView.as_view()),
+    path("", views.MainPageView.as_view(), name="index"),
+    path("news/", views.NewsPageView.as_view(), name="news"),
+    path("coursers/", views.CoursesPageView.as_view(), name="courses_list"),
+    path("contacts/", views.ContactsPageView.as_view(), name="contacts"),
+    path("doc_site/", views.DocSitePageView.as_view(), name="doc_site"),
+    path("login/", views.LoginPageView.as_view(), name="login"),
 ]
