d768361ebe98e242ff9a0c2445772d228fc69d77
Aleksandr <alexander_sheykin@mail.ru>
2022-09-15 18:38:29 +0300

    Lesson-8 Tests

--- mainapp/tests.py
+++ mainapp/tests.py
@@ -1,41 +1,10 @@
-# Create your tests here.
-import pickle
 from http import HTTPStatus
-from unittest import mock
 
-from django.conf import settings
-from django.contrib.staticfiles.testing import StaticLiveServerTestCase
-from django.core import mail as django_mail
 from django.test import Client, TestCase
 from django.urls import reverse
-from selenium.webdriver.common.by import By
-from selenium.webdriver.firefox.webdriver import WebDriver
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.support.ui import WebDriverWait
 
 from authapp import models as authapp_models
 from mainapp import models as mainapp_models
-from mainapp import tasks as mainapp_tasks
-
-
-class TestCoursesWithMock(TestCase):
-    fixtures = (
-        "authapp/fixtures/001_user_admin.json",
-        "mainapp/fixtures/002_courses.json",
-        "mainapp/fixtures/003_lessons.json",
-        "mainapp/fixtures/004_teachers.json",
-    )
-
-    def test_page_open_detail(self):
-        course_obj = mainapp_models.Courses.objects.get(pk=1)
-        path = reverse("mainapp:courses_detail", args=[course_obj.pk])
-        with open("mainapp/fixtures/005_feedback_list_1.bin", "rb") as inpf, mock.patch(
-            "django.core.cache.cache.get"
-        ) as mocked_cache:
-            mocked_cache.return_value = pickle.load(inpf)
-            result = self.client.get(path)
-            self.assertEqual(result.status_code, HTTPStatus.OK)
-            self.assertTrue(mocked_cache.called)
 
 
 class TestNewsPage(TestCase):
@@ -127,6 +96,10 @@ class TestNewsPage(TestCase):
         self.assertTrue(news_obj.deleted)
 
 
+import pickle
+from unittest import mock
+
+
 class TestCoursesWithMock(TestCase):
     fixtures = (
         "authapp/fixtures/001_user_admin.json",
@@ -147,6 +120,11 @@ class TestCoursesWithMock(TestCase):
             self.assertTrue(mocked_cache.called)
 
 
+from django.core import mail as django_mail
+
+from mainapp import tasks as mainapp_tasks
+
+
 class TestTaskMailSend(TestCase):
     fixtures = ("authapp/fixtures/001_user_admin.json",)
 
@@ -155,6 +133,14 @@ class TestTaskMailSend(TestCase):
         user_obj = authapp_models.CustomUser.objects.first()
         mainapp_tasks.send_feedback_mail({"user_id": user_obj.id, "message": message_text})
         self.assertEqual(django_mail.outbox[0].body, message_text)
+
+
+from django.conf import settings
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
+from selenium.webdriver.common.by import By
+from selenium.webdriver.firefox.webdriver import WebDriver
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
 
 
 class TestNewsSelenium(StaticLiveServerTestCase):
